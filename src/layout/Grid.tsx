a554ab593650061d1d522a860115c2c86bfb1b25
Khant Si Thu <khantsithu.work@gmail.com>
2024-04-20 02:29:43 +0630

    add one column in grid

--- src/layout/Grid.tsx
+++ src/layout/Grid.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Grid ({ children, title }) {
+function Grid ({ children, title } : { children : any, title : string}) {
   return (
     <article className='space-y-6'>
       <div className='w-full'>
         <h1 className='text-xl font-light tracking-wide'>{title}</h1>
       </div>
-      <div className='grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
+      <div className='grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {children}
       </div>
     </article>
